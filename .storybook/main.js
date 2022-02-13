e71820b89d82df223cb204e20140112b57e34923
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2022-02-13 10:34:04 +0900

    chapter2 end

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  "stories": ['../src/components/**/*.stories.js'],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
   "framework": "@storybook/react",
   "core": {
